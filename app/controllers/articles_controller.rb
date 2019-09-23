fbd4a6e3da4989da175b15708b5d6ba5d5f54b58
mahesh Paruchuri <saisivap289@gmail.com>
2019-09-23 22:45:47 +0700

    new_alpha-blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  logger = Logger.new STDOUT
   def home
 
   end
@@ -7,22 +8,30 @@ class ArticlesController < ApplicationController
   end
   def new
     @article=Article.new
+
+    # logger.info" article"
+    # logger.info @article
   end
   def create
 #render plain: params[:article].inspect
+    # logger.info article_params
     @article=Article.new(article_params)
-    respond_to do |f|
+    # respond_to do |f|
       if @article.save
-        #flash[:notice]="Article was sucessfully created"
-         #redirect_to articles_path/(@article)
-         f.html(redirect_to articles_path(@article),notice: 'now it is created ')
+        flash[:notice]="Article was sucessfully created"
+         redirect_to articles_path(@article)
+         # f.html(redirect_to articles_path(@article),notice: 'now it is created ')
       else
         render 'new'
       end
-    end
+    # end
   end
   def show
-    @articles=Article.find(params[:id])
+
+    @article=Article.find(params[:format])
+    # logger = Logger.new STDOUT
+    logger.info"show articleinfo"
+    logger.info @article
   end
   private
     def article_params
