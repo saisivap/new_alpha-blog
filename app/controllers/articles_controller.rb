a82a449b8f0b3fdcf6dd68865a6452ed94ac021f
mahesh Paruchuri <saisivap289@gmail.com>
2019-09-23 22:05:34 +0700

    somthig

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,19 @@ class ArticlesController < ApplicationController
   def create
 #render plain: params[:article].inspect
     @article=Article.new(article_params)
-    if @article.save
-       redirect_to articles_path(@article)
-    else
-      redirect_to 'new'
+    respond_to do |f|
+      if @article.save
+        #flash[:notice]="Article was sucessfully created"
+         #redirect_to articles_path/(@article)
+         f.html(redirect_to articles_path(@article),notice: 'now it is created ')
+      else
+        render 'new'
       end
+    end
   end
-
+  def show
+    @articles=Article.find(params[:id])
+  end
   private
     def article_params
         params.require(:article).permit(:title,:description)
