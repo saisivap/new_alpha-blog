56099cf148e0caadec7938eb8869149e81c592ce
mahesh Paruchuri <saisivap289@gmail.com>
2019-09-26 02:22:41 +0700

    add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   def new
     @user=User.new
   end
+  def index
+    @user=User.all
+  end
   def create
     # debugger
     @user=User.new(user_params)
