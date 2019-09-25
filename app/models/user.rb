f2dbca1d560444693f42c40d27be721baa2948ca
mahesh Paruchuri <saisivap289@gmail.com>
2019-09-25 15:27:23 +0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-
+  validates :username,presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {minimum: 3,maximum: 25}
+  Valid_email_checker=/\A(\S+)@(.+)\.(\S+)\z/i
+  validates :email,presence: true,
+            uniqueness: {case_sensitive: false},
+            format: {with: Valid_email_checker}
 end
