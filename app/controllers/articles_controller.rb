47e2209232cae91d23efed654c791203e6e72b4e
mahesh Paruchuri <saisivap289@gmail.com>
2019-09-26 19:11:19 +0700

    Admin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,7 +69,7 @@ class ArticlesController < ApplicationController
         params.require(:article).permit(:title,:description)
     end
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin
       flash[:danger]="you can only edit and delete your articles"
       redirect_to root_path
     end
